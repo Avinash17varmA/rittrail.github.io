0509dd6590188ec727074c3cf6ac3c999b57ec02
Avinash17varmA <102647346+Avinash17varmA@users.noreply.github.com>
2022-03-30 19:00:47 +0530

    Update age.c

--- age.c
+++ age.c
@@ -1,19 +1,1 @@
-#include<stdio.h>
 
-void main()
-{
-  int a;
-
-  printf("Please enter your age:");
-  scanf("%d,&a");
-
-  if (a > 18)
-  {
-    printf("\n you are not eligible");
-  }
-  else if (18 < a)
-  {
-    printf("\n b is greater than a");
-  }
-  
-}
